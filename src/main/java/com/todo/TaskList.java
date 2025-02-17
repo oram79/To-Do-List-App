4f10dc26c01278d246a8350d08aaecbd0d7577ec
Logan Oram <162460537+oram79@users.noreply.github.com>
2025-02-17 11:26:00 -0330

    node made for task list

--- src/main/java/com/todo/TaskList.java
+++ src/main/java/com/todo/TaskList.java
@@ -2,3 +2,31 @@ package com.todo;
 
 public class TaskList {
 }
+    private class Node {
+        Task task;
+        Node next;
+
+        // Constructor
+        public Node(Task task) {
+            this.task = task;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    // Add task to linked list
+    public void addTask(String description) {
+        Task newTask = new Task(description);
+        Node newNode = new Node(newTask);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
