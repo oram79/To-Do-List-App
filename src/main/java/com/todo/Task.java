5bbf0100c7b4ecdd1928fd9dbb1f2407efc6b835
Logan Oram <162460537+oram79@users.noreply.github.com>
2025-02-17 11:08:20 -0330

    added task class

--- src/main/java/com/todo/Task.java
+++ src/main/java/com/todo/Task.java
@@ -1,4 +1,38 @@
 package com.todo;
 
 public class Task {
+    private String description;
+    private boolean completed;
+    private Task next;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+        this.next = null;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    public Task getNext() {
+        return next;
+    }
+
+    public void setNext(Task next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return description + " - " + (completed ? "Completed" : "Pending");
+    }
 }
